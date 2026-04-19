da009678d7bd0841898569d725baa61683e07d5a
Honk <hungnguyenviet204@gmail.com>
2026-04-19 22:18:49 +0700

    feat(user): add remove an user by id

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -4,57 +4,79 @@ import { UserRepository } from "@/api/user/user.repository";
 import { ServiceResponse } from "@/common/models/serviceResponse";
 import { logger } from "@/server";
 export class UserService {
-	private userRepository: UserRepository;
+  private userRepository: UserRepository;
 
-	constructor(repository: UserRepository = new UserRepository()) {
-		this.userRepository = repository;
-	}
+  constructor(repository: UserRepository = new UserRepository()) {
+    this.userRepository = repository;
+  }
 
-	// Retrieves all users from the database
-	async findAll(): Promise<ServiceResponse<User[] | null>> {
-		try {
-			const users = await this.userRepository.findAll();
-			if (!users || users.length === 0) {
-				return ServiceResponse.failure(
-					"No Users found",
-					null,
-					StatusCodes.NOT_FOUND,
-				);
-			}
-			return ServiceResponse.success<User[]>("Users found", users);
-		} catch (ex) {
-			const errorMessage = `Error finding all users: $${(ex as Error).message}`;
-			logger.error(errorMessage);
-			return ServiceResponse.failure(
-				"An error occurred while retrieving users.",
-				null,
-				StatusCodes.INTERNAL_SERVER_ERROR,
-			);
-		}
-	}
+  // Retrieves all users from the database
+  async findAll(): Promise<ServiceResponse<User[] | null>> {
+    try {
+      const users = await this.userRepository.findAll();
+      if (!users || users.length === 0) {
+        return ServiceResponse.failure(
+          "No Users found",
+          null,
+          StatusCodes.NOT_FOUND,
+        );
+      }
+      return ServiceResponse.success<User[]>("Users found", users);
+    } catch (ex) {
+      const errorMessage = `Error finding all users: $${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return ServiceResponse.failure(
+        "An error occurred while retrieving users.",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 
-	// Retrieves a single user by their ID
-	async findById(id: string): Promise<ServiceResponse<User | null>> {
-		try {
-			const user = await this.userRepository.findById(id);
-			if (!user) {
-				return ServiceResponse.failure(
-					"User not found",
-					null,
-					StatusCodes.NOT_FOUND,
-				);
-			}
-			return ServiceResponse.success<User>("User found", user);
-		} catch (ex) {
-			const errorMessage = `Error finding user with id ${id}:, ${(ex as Error).message}`;
-			logger.error(errorMessage);
-			return ServiceResponse.failure(
-				"An error occurred while finding user.",
-				null,
-				StatusCodes.INTERNAL_SERVER_ERROR,
-			);
-		}
-	}
+  // Retrieves a single user by their ID
+  async findById(id: string): Promise<ServiceResponse<User | null>> {
+    try {
+      const user = await this.userRepository.findById(id);
+      if (!user) {
+        return ServiceResponse.failure(
+          "User not found",
+          null,
+          StatusCodes.NOT_FOUND,
+        );
+      }
+      return ServiceResponse.success<User>("User found", user);
+    } catch (ex) {
+      const errorMessage = `Error finding user with id ${id}:, ${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return ServiceResponse.failure(
+        "An error occurred while finding user.",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async deleteById(id: string): Promise<ServiceResponse<number | null>> {
+    try {
+      const user = await this.userRepository.deleteById(id);
+      if (!user) {
+        return ServiceResponse.failure(
+          "User not found",
+          null,
+          StatusCodes.NOT_FOUND,
+        );
+      }
+      return ServiceResponse.success<number>("User deleted", user);
+    } catch (ex) {
+      const errorMessage = `Error finding user with id ${id}:, ${(ex as Error).message}`;
+      logger.error(errorMessage);
+      return ServiceResponse.failure(
+        "An error occurred while deleting an user.",
+        null,
+        StatusCodes.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
 }
 
 export const userService = new UserService();
